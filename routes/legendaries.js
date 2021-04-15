4dcdae77ad4770fc2f9f4b3f2d394ed2efd004ae
Silvio Souza <sjblu7@gmail.com>
2021-04-15 11:12:23 -0400

    clone original

--- routes/legendaries.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/LegendariesController');
-
-/* GET legendaries. */
-router.get('/', controller.index);
-
-module.exports = router;
